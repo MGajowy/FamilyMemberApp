d485dbee437533bc8951047ee1200f8cdc1862c1
michal.gajewski <michalpiotr.gajewski@gmail.com>
2022-09-19 14:01:43 +0200

    Amendments 5

--- src/main/java/pl/gajewski/FamilyMemberApp/service/FamilyMemberAppService.java
+++ src/main/java/pl/gajewski/FamilyMemberApp/service/FamilyMemberAppService.java
@@ -2,6 +2,7 @@ package pl.gajewski.FamilyMemberApp.service;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import pl.gajewski.FamilyMemberApp.dto.FamilyMember;
@@ -10,8 +11,11 @@ import pl.gajewski.FamilyMemberApp.dto.FamilyMember;
 @Service
 @Log4j2
 public class FamilyMemberAppService {
-    private static final String URI_CREATE_MEMBER_FAMILY = "http://family-database-app:8022/db/createMemberFamily";
-    private static final String URI_SEARCH_MEMBER_FAMILY = "http://family-database-app:8022/db/searchMemberFamily/";
+    @Value("${familyMemberAppService.uriCreateMemberFamily}")
+    private String uriCreateMemberFamily;
+
+    @Value("${familyMemberAppService.uriSearchMemberFamily}")
+    private String uriSearchMemberFamily;
 
     private final RestTemplate restTemplate;
 
@@ -22,17 +26,17 @@ public class FamilyMemberAppService {
 
     public Boolean createFamilyMember(FamilyMember familyMember) {
         try {
-            restTemplate.postForObject(URI_CREATE_MEMBER_FAMILY, familyMember, FamilyMember.class);
-            log.info("Odpytanie serwisu: " + URI_CREATE_MEMBER_FAMILY);
+            restTemplate.postForObject(uriCreateMemberFamily, familyMember, FamilyMember.class);
+            log.info("Odpytanie serwisu: " + uriCreateMemberFamily);
             return true;
         } catch (Exception e) {
-            log.info("Wykonanie URI: " + URI_CREATE_MEMBER_FAMILY + " nie powiodło się " + e.getMessage());
+            log.info("Wykonanie URI: " + uriCreateMemberFamily + " nie powiodło się " + e.getMessage());
             return false;
         }
     }
 
     public FamilyMember[] searchFamilyMember(Integer id) {
-        FamilyMember[] result = restTemplate.getForObject(URI_SEARCH_MEMBER_FAMILY + id.toString(), FamilyMember[].class);
+        FamilyMember[] result = restTemplate.getForObject(uriSearchMemberFamily + id.toString(), FamilyMember[].class);
         log.info("Pobrano dane rodziny o id: " + id.toString());
         return result;
     }
